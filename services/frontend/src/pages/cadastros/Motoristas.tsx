6c2f97fccfdef0fefd441488266de04fe110576d
Luan <luanoliveira0710@gmail.com>
2024-12-17 15:39:54 -0300

    Adiciona página de cadastro de motoristas
    Criei a página de cadastro de motoristas com um componente de placeholder.

---
+++ services/frontend/src/pages/cadastros/Motoristas.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function Motoristas() {
+  return <PagePlaceholder title="Cadastro de Motoristas" />;
+} 
