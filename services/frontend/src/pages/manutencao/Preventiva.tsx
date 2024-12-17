af71ab1165fb29cb27e9ed5815d8ac91634306d8
Luan <luanoliveira0710@gmail.com>
2024-12-17 16:24:30 -0300

    Adiciona página de Manutenção Preventiva
    Criei uma página temporária para Manutenção Preventiva, utilizando o componente PagePlaceholder.  A página exibe o título "Manutenção Preventiva".

---
+++ services/frontend/src/pages/manutencao/Preventiva.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function ManutencaoPreventiva() {
+  return <PagePlaceholder title="Manutenção Preventiva" />;
+} 
