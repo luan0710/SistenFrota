b45b65ca96b8fb576151eb62247e2384096ab94a
Luan <luanoliveira0710@gmail.com>
2024-12-17 16:11:23 -0300

    Adiciona página de Manutenção Corretiva
    Criei a página de Manutenção Corretiva com um marcador de posição temporário.

---
+++ services/frontend/src/pages/manutencao/Corretiva.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function ManutencaoCorretiva() {
+  return <PagePlaceholder title="Manutenção Corretiva" />;
+} 
