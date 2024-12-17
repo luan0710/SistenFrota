46b8678c30fd2eed06f4d49eaf6fe2225f51f6b2
Luan <luanoliveira0710@gmail.com>
2024-12-17 15:34:18 -0300

    Adiciona página de configurações ao frontend
    Este commit cria o arquivo 'Configuracoes.tsx', que implementa a página de configurações do sistema. A página utiliza o componente 'PagePlaceholder' para exibir um título informando que as configurações estão em desenvolvimento. Essa adição é parte do progresso na implementação das funcionalidades do aplicativo, melhorando a estrutura e a navegação do sistema.

---
+++ services/frontend/src/pages/Configuracoes.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../components/PagePlaceholder';
+
+export default function Configuracoes() {
+  return <PagePlaceholder title="Configurações do Sistema" />;
+} 
