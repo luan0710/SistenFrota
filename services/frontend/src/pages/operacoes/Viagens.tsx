60a4d73e07c0532558aafc024b6723a3025dcea3
Luan <luanoliveira0710@gmail.com>
2024-12-17 16:37:29 -0300

    Adiciona página de placeholder para Operações de Viagens
    Criei um placeholder para a página de Operações de Viagens, exibindo um título temporário.  Isso permitirá navegação até a página enquanto seu conteúdo ainda está em desenvolvimento.

---
+++ services/frontend/src/pages/operacoes/Viagens.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function Viagens() {
+  return <PagePlaceholder title="Operações de Viagens" />;
+} 
