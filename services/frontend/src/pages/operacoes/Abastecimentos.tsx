82356a2be2dcad658a4156852d74d3f96091b39e
Luan <luanoliveira0710@gmail.com>
2024-12-17 16:29:37 -0300

    Adiciona página de Operações de Abastecimentos
    Criei uma página placeholder para as Operações de Abastecimentos. Ela ainda não possui funcionalidades, mas já está acessível no menu.

---
+++ services/frontend/src/pages/operacoes/Abastecimentos.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function Abastecimentos() {
+  return <PagePlaceholder title="Operações de Abastecimentos" />;
+} 
