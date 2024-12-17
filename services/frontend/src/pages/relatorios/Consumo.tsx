fa2481c0285adaa8be3c7eb20e072c5755e411a0
Luan <luanoliveira0710@gmail.com>
2024-12-17 16:41:31 -0300

    Adiciona página de Relatório de Consumo
    Criei a página de Relatório de Consumo com um placeholder.

---
+++ services/frontend/src/pages/relatorios/Consumo.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function RelatorioConsumo() {
+  return <PagePlaceholder title="Relatório de Consumo" />;
+} 
