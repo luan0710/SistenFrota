9608fad1d01cd2521796b5dae5a5d0c3db984a93
Luan <luanoliveira0710@gmail.com>
2024-12-17 16:13:00 -0300

    Adiciona página de Histórico de Manutenção
    Criei um novo componente para exibir o histórico de manutenção, utilizando um placeholder por enquanto.

---
+++ services/frontend/src/pages/manutencao/Historico.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function ManutencaoHistorico() {
+  return <PagePlaceholder title="Histórico de Manutenção" />;
+} 
