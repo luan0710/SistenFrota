7d1de7961d70400bfac7779504a9c9fd62eab1ef
Luan <luanoliveira0710@gmail.com>
2024-12-17 16:41:53 -0300

    Adiciona página de relatório de custos
    Criei um novo componente para exibir o relatório de custos. Atualmente, ele exibe um marcador de posição, mas servirá como base para a implementação futura do relatório completo.

---
+++ services/frontend/src/pages/relatorios/Custos.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function RelatorioCustos() {
+  return <PagePlaceholder title="Relatório de Custos" />;
+} 
