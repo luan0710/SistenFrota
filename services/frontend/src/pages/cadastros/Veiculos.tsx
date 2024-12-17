d921b96ce7c41b59cc495b97108ecd561f0e898a
Luan <luanoliveira0710@gmail.com>
2024-12-17 15:40:06 -0300

    Adiciona página de cadastro de veículos
    Criei a página de Cadastro de Veículos com um marcador de posição temporário.  A página ainda não possui funcionalidades, mas já está acessível na interface.

---
+++ services/frontend/src/pages/cadastros/Veiculos.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagePlaceholder from '../../components/PagePlaceholder';
+
+export default function Veiculos() {
+  return <PagePlaceholder title="Cadastro de Veículos" />;
+} 
